57d44e42fdb7e227e5e82afb1a30569dbf4bb17e
Yassine REZZIK <yassine.rezzik@efrei.net>
2025-12-09 12:25:02 +0100

    add a substance without quantity / tdd green refactor

--- laboratory.js
+++ laboratory.js
@@ -1,23 +1,25 @@
 class Laboratory {
-    substancesList = [];
     constructor(substances) {
         if (!Array.isArray(substances) || substances.length === 0) {
             throw new Error('La liste des substances doit etre une liste non vide');
         }
+        this.substancesMap = new Map();
+
         for (let substance of substances) {
-            if (this.substancesList.includes(substance)) {
+            if (this.substancesMap.has(substance)) {
                 throw new Error('La liste des substances doit contenir des elements uniques');
             }
-            this.substancesList.push(substance);
+            this.substancesMap.set(substance, 0);
         }
     }
 
     getQuantity(substance) {
-        if (!this.substancesList.includes(substance)) {
+        if (!this.substancesMap.has(substance)) {
             throw new Error('Substance inconnue');
         }
-        return 0;
+        return this.substancesMap.get(substance);
     }
+
     add(substance, quantity) {
         if (substance == null) {
             throw new Error('Une substance est requise');
@@ -25,11 +27,11 @@ class Laboratory {
         if (quantity == null) {
             throw new Error('Impossible d\'ajouter une substance sans quantite');
         }
-        if (!this.substancesList.includes(substance)) {
-            throw new Error('Substance inconnue');
-        }
         if (quantity < 0) {
             throw new Error('Impossible d\'ajouter une quantite negative');
+        }
+        if (!this.substancesMap.has(substance)) {
+            throw new Error('Substance inconnue');
         }
     }
 }
